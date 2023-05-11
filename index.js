c57abaa6510e219c4957927ac56a7c924b96b4bc
Mendituber <118443598+Mendituber@users.noreply.github.com>
2023-05-11 12:16:04 +0200

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//  COMPROBACIÓN DE SI ESTÁ JUGANDO (FALTA TERMINAR)------------------------------------------------
+
+// if(sessionStorage['_data_']){
+//     location.ref="juego.html";
+// }else{
+//     location.href='index.html';
+// }
+//  COMPROBACIÓN DE SI ESTÁ JUGANDO (FALTA TERMINAR)------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+});
